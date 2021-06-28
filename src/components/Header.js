1eb52b97d66bcb146ebc74a01171bbc6d75b6fc7
Sarmad Sangi <sarmadsangi@gmail.com>
2021-06-28 13:34:56 +1000

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => (
   <>
     <Helmet>
       <meta charSet="utf-8" />
-      <title>SharingBeez | A community for sharing</title>
+      <title>Eat My Garden | A community for sharing</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content="Creating a supportive community by sharing skills, ideas and fresh produce to encourage self-sustaining behaviours using edible garden on an open space."/>
       <meta name="keywords" content="community garden, point cook, melbourne, sharing skills, fresh produce"/>
